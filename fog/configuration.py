6891dc0f57e212c6461a5f999e5729584e8af4ce
Raghav sidhanti <sidhantirc@gmail.com>
2014-04-05 11:38:06 -0700

    #1: Modified join in Conf. Added arguments as arrays

--- fog/configuration.py
+++ fog/configuration.py
@@ -5,21 +5,21 @@ class Conf(object):
 
     # standard
     HOME = '.fog'
-    LOGS = ''.join(HOME, '/logs')
-    CHECKOUT = ''.join(HOME, '/checkout')
+    LOGS = ''.join([HOME, '/logs'])
+    CHECKOUT = ''.join([HOME, '/checkout'])
 
     # google
     GOOGLE_DRIVE = 'googledrive'
-    GOOGLE_DRIVE_HOME = ''.join(HOME, '/', GOOGLE_DRIVE)
-    GOOGLE_DRIVE_CREDENTIALS = ''.join(GOOGLE_DRIVE_HOME, '/auth.dat')
+    GOOGLE_DRIVE_HOME = ''.join([HOME, '/', GOOGLE_DRIVE])
+    GOOGLE_DRIVE_CREDENTIALS = ''.join([GOOGLE_DRIVE_HOME, '/auth.dat'])
 
     # sky drive
     #SKY_DRIVE = 'skydrive'
-    #SKY_DRIVE_HOME = ''.join(HOME, '/', SKY_DRIVE)
+    #SKY_DRIVE_HOME = ''.join([HOME, '/', SKY_DRIVE])
 
     # drop box
     #DROP_BOX = 'dropbox'
-    #DROP_BOX_HOME = ''.join(HOME, '/', DROP_BOX)
+    #DROP_BOX_HOME = ''.join([HOME, '/', DROP_BOX])
 
     # available branches
     BRANCHES = [GOOGLE_DRIVE]
