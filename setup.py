44e238aa02d2c6ae5163e8081dfaa35760bb1100
Raghav sidhanti <sudohippie@users.noreply.github.com>
2014-04-23 11:18:55 -0700

    fixed #24: setup.py and modified project structure.

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'Fog',
+    'description': 'Fog is an open-source Git style tool to access files on cloud drives',
+    'author': 'Raghav Sidhanti',
+    'author_email': 'sudohippie@gmail.com',
+    'url': 'https://github.com/sudohippie/fog',
+    'download_url': 'https://github.com/sudohippie/fog',
+    'version': '1.0.0-beta.1',
+    'install_requires': ['nose', 'google-api-python-client'],
+    'packages': ['fog'],
+    'scripts': [],
+    'license': 'The MIT License'
+}
+
+setup(**config)
---
+++ tests/message_tests.py
@@ -0,0 +1,16 @@
+__author__ = 'Raghav Sidhanti'
+
+from nose.tools import *
+import fog
+
+
+def setup():
+    pass
+
+
+def teardown():
+    pass
+
+
+def test_basic():
+    pass
